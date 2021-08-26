3f3072765bb55572637f5d95ed6c1ada8fb57f99
Madhoolika <madhoolikab@gmail.com>
2021-08-26 11:46:52 +0530

    Forming the structure tensor for input image

--- harris_corner_detector.py
+++ harris_corner_detector.py
@@ -1,6 +1,35 @@
 from skimage import io
-import scipy
+from scipy.ndimage import gaussian_filter
+from scipy.signal import convolve2d
 
 img = io.imread('PCB.jpg', as_gray = True)
-img1 = io.imread('PCB.jpg')
-smoothened_img = scipy.ndimage.gaussian_filter(img, 1)
+img1 = io.imread('PCB.jpg') # placeholder to indicate the corners later
+
+smoothened_img = gaussian_filter(img, 1)
+
+# gradient along x-axis
+sobel_x = np.array([[-1, 0, 1],
+                    [-2, 0, 2],
+                    [-1, 0, 1]])
+
+# gradient along y-axis
+sobel_y = np.array([[-1, -2, -1],
+                    [0, 0 ,0],
+                    [1, 2, 1]])
+
+# Forming the structure tensor for the input image
+grad_x = convolve2d(smoothened_img, sobel_x, mode = 'same', boundary = 'symm')
+grad_y = convolve2d(smoothened_img, sobel_y, mode = 'same', boundary = 'symm')
+grad_x_square = grad_x * grad_x
+grad_y_square = grad_y * grad_y
+grad_x_y = grad_x * grad_y
+
+smoothened_grad_x_square = gaussian_filter(grad_x_square, 1)
+smoothened_grad_y_square = gaussian_filter(grad_y_square, 1)
+smoothened_grad_x_y = gaussian_filter(grad_x_y, 1)
+
+ad = smoothened_grad_x_square * smoothened_grad_y_square
+bc = smoothened_grad_x_y * smoothened_grad_x_y
+trace_matrix = smoothened_grad_x_square + smoothened_grad_y_square
+trace_matrix_square = trace_matrix * trace_matrix
+det_J = ad-bc
